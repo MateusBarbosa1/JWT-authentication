e5f220a95d277f3bcda96ab0bf58aab4b8c71113
Mateus Barbosa <77073090+MateusBarbosa1@users.noreply.github.com>
2024-03-10 02:20:27 +0000

    Create and config server

---
+++ prisma/migrations/20240310021837_create_table_users/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
